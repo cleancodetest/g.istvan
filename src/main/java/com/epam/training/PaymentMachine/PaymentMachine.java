8952a3e83c38a6b1ad25ea5c8ec5266539e21da0
Musak Istvan <Istvan.Musak@epam.com>
2014-05-09 12:43:29 +0200

    add default compiler

--- src/main/java/com/epam/training/PaymentMachine/PaymentMachine.java
+++ src/main/java/com/epam/training/PaymentMachine/PaymentMachine.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
 
 public class PaymentMachine {
 	private Map<Integer, Ticket> tickets;
